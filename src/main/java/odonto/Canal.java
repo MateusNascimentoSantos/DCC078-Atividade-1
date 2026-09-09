3156bbeafc68e9900d8cf63f4bc3efded32528a1
Mateus Santos <mateusclaudinodossantos@hotmail.com>
2026-09-09 17:59:05 -0300

    canal e rest

--- src/main/java/odonto/Canal.java
+++ src/main/java/odonto/Canal.java
@@ -1,4 +1,16 @@
 package odonto;
 
-public class Canal {
+public class Canal implements Procedimento {
+
+    public String getNome() {
+        return "Canal";
+    }
+
+    public int getQuantidadeMaxima() {
+        return 32;
+    }
+
+    public double calcularValor(int quantidade) {
+        return 700.0 * quantidade;
+    }
 }
--- src/main/java/odonto/Restauracao.java
+++ src/main/java/odonto/Restauracao.java
@@ -1,4 +1,20 @@
 package odonto;
 
-public class Restauracao {
+public class Restauracao implements Procedimento {
+
+    public String getNome() {
+        return "Restauracao";
+    }
+
+    public int getQuantidadeMaxima() {
+        return 32;
+    }
+
+    public double calcularValor(int quantidade) {
+        double total = 180.0 * quantidade;
+        if (quantidade >= 4) {
+            total = total * 0.9;
+        }
+        return total;
+    }
 }
